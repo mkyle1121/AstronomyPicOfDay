4b89391a0a3449fc97330624aeda205224393405
Mike kyle <mkyle1121@hotmail.com>
2022-08-07 17:33:00 -0500

    .

--- APOD/APOD/obj/Debug/net6.0-windows/App.g.i.cs
+++ APOD/APOD/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace APOD {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/APOD;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/APOD;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- APOD/APOD/obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
+++ APOD/APOD/obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
@@ -62,7 +62,7 @@ namespace APOD {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/APOD;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/APOD;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
